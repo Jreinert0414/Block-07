d8265c72468d31819f83a6a9f178a4afca9366d3
Jreinert0414 <jim.reinert.ii@gmail.com>
2024-03-04 13:58:25 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+//make string- put have received this message because you have been chosen to open an important vault. Here is the secret combination:
+// define each variable 
+//let comb1 be 10
+// let comb2 be 40
+// let comb3 be 39
+//comment each line to define objective
+//create alert displaying codes 
+
+
+alert(' You have received this message because you have been chosen to open an important vault. Here is the secret combination:')
+//makes 1st number in combo 10
+const comb1 = (100/10);
+console.log(comb1)
+
+let comb2 = (4*comb1)
+console.log (comb2)
+
+let comb3 = (comb2 - 1)
+console.log (comb3)
+
+alert (comb1 + ' ' + comb2 + ' ' + comb3 )
