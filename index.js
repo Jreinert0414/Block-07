5d9b8557f2fe5dd5e8a7a72cef86874b8b6af656
Jreinert0414 <jim.reinert.ii@gmail.com>
2024-03-04 14:00:45 -0600

    added comments

--- index.js
+++ index.js
@@ -6,16 +6,20 @@
 //comment each line to define objective
 //create alert displaying codes 
 
-
+//displays message upon entering page 
 alert(' You have received this message because you have been chosen to open an important vault. Here is the secret combination:')
+
 //makes 1st number in combo 10
 const comb1 = (100/10);
 console.log(comb1)
 
+//makes 2nd combination value 40
 let comb2 = (4*comb1)
 console.log (comb2)
 
+//makes 3rd combination value 39
 let comb3 = (comb2 - 1)
 console.log (comb3)
 
+//semd final alert with full combination
 alert (comb1 + ' ' + comb2 + ' ' + comb3 )
